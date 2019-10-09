615ea2a2115077e05fcad21d6a8aaca853a8c513
erik.dahlerup@gmail.com <erik.dahlerup@gmail.com>
2019-10-09 21:17:09 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Thread.Sleep(5000);
+        }
+    }
+}
